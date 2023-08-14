ab96fe1ffc67005629302ad89703ac0cfcc9660d
Rahul Ranjan Nayak <rahulnk04@gmail.com>
2023-08-14 17:07:28 +0530

    Home page carousel

--- components/home/carousel/CarouselBody.tsx
+++ components/home/carousel/CarouselBody.tsx
@@ -1,6 +1,6 @@
-import IhomeCarouselData from '@/interfaces/IhomeCarouselData';
-import React from 'react';
 import styles from './Carousel.module.scss';
+import React from 'react';
+import IhomeCarouselData from '@/interfaces/IhomeCarouselData';
 import { baseImageUrl, factryFactIconLogo } from '@/utils/apiConfig';
 
 type Props = {
@@ -19,25 +19,25 @@ const CarouselBody = (props: Props) => {
             <div className={`${styles['navbar-item']} ${styles['p-1']}`}>
               <img
                 className={`${styles['ff-switcher-text']} ${styles['img']}`}
-                src={factryFactIconLogo}
-                alt={'factryfact logo'}
+                src={factryFactIconLogo as string}
+                alt={'factryfact logo' as string}
               />
             </div>
             <h1
               className={`${styles['title']} ${styles['main-title']} ${styles['dark-text']} ${styles['text-bold']} ${styles['is-2']}`}
             >
-              {item.title}
+              {item.title as string}
             </h1>
             <h2
               className={`${styles['subtitle']} ${styles['is-5']} ${styles['no-margin-bottom']} ${styles['body-color']}`}
             >
-              {item.desc}
+              {item.desc as string}
             </h2>
             <br />
           </div>
           <div className={`${styles['column']} ${styles['is-7']}`}>
             <figure className={styles['image']}>
-              <img className={styles['img']} src={baseImageUrl + item.img} alt={item.title} />
+              <img className={styles['img']} src={(baseImageUrl + item.img) as string} alt={item.title as string} />
             </figure>
           </div>
         </div>
